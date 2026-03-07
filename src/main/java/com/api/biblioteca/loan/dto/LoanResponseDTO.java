b7bb31ed2feae326690479fe6a0af311f6d3f2b2
Alvaro Tajetti <talhettialvaro@gmail.com>
2026-03-07 13:51:09 -0300

    feat: adicionando entidade do emprestimo, dtos e repository

---
+++ src/main/java/com/api/biblioteca/loan/dto/LoanResponseDTO.java
@@ -0,0 +1,16 @@
+package com.api.biblioteca.loan.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class LoanResponseDTO {
+    private UUID id;
+    private UUID clientId;
+    private UUID bookId;
+    private LocalDateTime loanDate;
+    private LocalDateTime returnDate;
+
+}
